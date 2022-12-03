4aaa855b631495670581d357ce8d112b6d223791
ilya Kupriyanov <ilya-kyp@yandex.ru>
2022-12-03 12:26:56 +0300

    initialize checker board.
    moving chechers

---
+++ CheckersDemo/Client/Data/Checker.cs
@@ -0,0 +1,23 @@
+﻿namespace CheckersDemo.Client.Data;
+
+public class Checker
+{
+    public Checker(int row, int column, CheckerDirection direction, string color)
+    {
+        Row = row;
+        Column = column;
+        Direction = direction;
+        Color = color;
+    }
+
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public CheckerDirection Direction { get; set; }
+    public string Color { get; set; }
+
+}
+
+public enum CheckerDirection
+{
+    Down, Up, Both
+}
